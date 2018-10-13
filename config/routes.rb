a6d4bb9ea6a40f89d9c38ba1fba86a094fa67698
Narongsak <narongsak.chobsri@gmail.com>
2018-10-13 16:34:47 +0700

    Run fae install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  namespace :admin do
+  end
+  # mount Fae below your admin namespec
+  mount Fae::Engine => '/admin'
+
   root to: 'landing#index'
 end
