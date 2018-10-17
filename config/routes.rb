c7e65ffaaf1b87a3b5bab0354ca58a7eb485a1e1
narongsakjob <narongsak.chobsri@gmail.com>
2018-10-17 19:01:00 +0700

    Config route devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :customers
   namespace :admin do
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
+
+  devise_for :customers,
+             only: %i[registrations sessions passwords],
+             controllers: {
+               registrations: 'customers/registrations',
+               sessions: 'customers/sessions'
+             },
+             path: 'customers'
 
   root to: 'landing#index'
 end
